fb6090ed5905dd83179157f3452a4f6f1a82c326
Денис <speedran1602@gmail.com>
2025-04-20 18:44:22 +0300

    Готовый

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,21 @@
 // Галерея и лайтбоксы от Fancybox
-import { Fancybox } from '@fancyapps/ui';
-import '@fancyapps/ui/dist/fancybox/fancybox.css';
+import { Fancybox } from "@fancyapps/ui";
+import "@fancyapps/ui/dist/fancybox/fancybox.css";
 
-Fancybox.bind('[data-fancybox]', {
-	// Your custom options
+Fancybox.bind("[data-fancybox]", {
+  // Your custom options
 });
 
 // Мобильная навигация
-import mobileNav from './modules/mobile-nav.js';
+import mobileNav from "./modules/mobile-nav.js";
 mobileNav();
+
+const selects = document.querySelectorAll(".footer__sos1al-nav");
+
+for (const select of selects) {
+  const trigger = select.querySelector(".select-trigger");
+
+  trigger.addEventListener("click", () => {
+    select.classList.toggle("active");
+  });
+}
--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -1,14 +1,24 @@
 function mobileNav() {
-	// Mobile nav button
-	const navBtn = document.querySelector('.mobile-nav-btn');
-	const nav = document.querySelector('.mobile-nav');
-	const menuIcon = document.querySelector('.nav-icon');
+  const navBtn = document.querySelector(".burger-button");
+  const nav = document.querySelector(".mobile-nav");
 
-	navBtn.onclick = function () {
-		nav.classList.toggle('mobile-nav--open');
-		menuIcon.classList.toggle('nav-icon--active');
-		document.body.classList.toggle('no-scroll');
-	};
+  navBtn.onclick = function() {
+    nav.classList.toggle("open");
+    navBtn.classList.toggle("active");
+    document.body.classList.toggle("no-scroll");
+  };
+
+  document.addEventListener("click", function(event) {
+    if (
+      !nav.contains(event.target) &&
+      !navBtn.contains(event.target) &&
+      nav.classList.contains("open")
+    ) {
+      nav.classList.remove("open");
+      navBtn.classList.remove("active");
+      document.body.classList.remove("no-scroll");
+    }
+  });
 }
 
 export default mobileNav;
